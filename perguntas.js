72958d71b73474a0392e7ed612b5cb148edea402
kauan2707 <kauan.orias@escola.pr.gov.br>
2025-08-20 08:22:16 -0300

    aaAaa

--- perguntas.js
+++ perguntas.js
@@ -1,41 +1,53 @@
 criaCartao(
     'jogos',
-    'Em que ano lançou The Binding of Isaac?',
-    'Em 2011!'
+    'Qual jogo de 2018 da FromSoftware é popularmente conhecido por sua dificuldade e por ter uma espada gigante como arma principal?',
+    'Dark Souls!'
 )
 
 criaCartao(
     'jogos',
-    'Quantos Far Cry existem na franquia principal?',
-    'Existem 6 excluindo os jogos independentes'
+    'Em qual jogo da franquia "The Elder Scrolls" o jogador explora a província de Skyrim?',
+    'The Elder Scrolls V: Skyrim!'
 )
 
 criaCartao(
-'jogos',
-'Qual jogo popularmente conhecido no Brasil como "jogo da cobrinha"?',
-'Snake!'
+    'jogos',
+    'Qual o nome do protagonista de God of War que, em sua nova saga, se muda para a Escandinávia e tem um filho chamado Atreus?',
+    'Kratos!'
 )
 
 criaCartao(
-'jogos',
-'Qual o nome do famoso mascote azul da SEGA?',
-'Sonic!'
+    'jogos',
+    'Em "Street Fighter", qual personagem é conhecido por seu famoso golpe "Hadoken"?',
+    'Ryu!'
 )
 
 criaCartao(
-'jogos',
-'Qual o nome do protagonista de The Legend of Zelda?',
-'Link!'
+    'jogos',
+    'Qual jogo da Nintendo, lançado em 1985, introduziu o encanador Mario e é amplamente considerado um dos jogos mais influentes da história?',
+    'Super Mario Bros.!'
 )
 
 criaCartao(
-'jogos',
-'Qual a produtora do famoso jogo Red Dead Redemption 2?',
-'Rockstar Games!'
+    'jogos',
+    'Qual jogo de 2015, da CD Projekt Red, é baseado nos livros do autor polonês Andrzej Sapkowski e segue a história do bruxo Geralt de Rívia?',
+    'The Witcher 3: Wild Hunt!'
 )
 
 criaCartao(
-'jogos',
-'Em que ano foi lançado o primeiro Grand Theft Auto?',
-'Em 1997!'
+    'jogos',
+    'Em "Minecraft", qual o nome da criatura hostil que explode quando se aproxima do jogador?',
+    'Creeper!'
+)
+
+criaCartao(
+    'jogos',
+    'Qual o nome do jogo de 2013 da Naughty Dog que conta a história de Joel e Ellie em um mundo pós-apocalíptico infestado por zumbis mutantes?',
+    'The Last of Us!'
+)
+
+criaCartao(
+    'jogos',
+    'Qual o nome da franquia de jogos de terror da Capcom que apresenta os personagens Leon S. Kennedy e Jill Valentine e foi responsável por popularizar o gênero "survival horror"?',
+    'Resident Evil!'
 )

0507029e4e312f26328f1d826bcbf6fc623f07ed
kauan2707 <kauan.orias@escola.pr.gov.br>
2025-08-20 08:09:12 -0300

    aaaa

--- perguntas.js
+++ perguntas.js
@@ -1,5 +1,23 @@
-criacartao(
-    'Jogos',
-    'Em qual ano lançou Isaac?',
-    '2014'
+criaCartao(
+    'jogos',
+    'Em que ano lançou The Binding of Isaac?',
+    'Em 2011!'
+)
+
+criaCartao(
+    'jogos',
+    'Quantos Far Cry existem na franquia principal?',
+    'Existem 6 excluindo os jogos independentes'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
 )
